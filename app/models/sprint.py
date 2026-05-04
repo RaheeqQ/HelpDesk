7fff4338c5c24bf391c1cbe199a218ee8c6e8c6a
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2026-05-05 00:49:40 +0300

    change sprint status to enum (planned, start, cmplete)

--- app/models/sprint.py
+++ app/models/sprint.py
@@ -1,6 +1,14 @@
 from sqlmodel import SQLModel, Field
 import uuid
 from datetime import datetime
+from enum import Enum
+from sqlalchemy import Column, String
+
+
+class SprintStatus(str, Enum):
+    planned = "planned"
+    active = "active"
+    completed = "completed"
 
 
 class Sprint(SQLModel, table=True):
@@ -9,7 +17,7 @@ class Sprint(SQLModel, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     project_id: str = Field(foreign_key="project.id")
     name: str
-    status: str
+    status: SprintStatus = Field(default=SprintStatus.planned, sa_column=Column(String, nullable=False))
     goal: str 
-    start_date: datetime
-    end_date: datetime
+    start_date: datetime | None = None
+    end_date: datetime | None = None
